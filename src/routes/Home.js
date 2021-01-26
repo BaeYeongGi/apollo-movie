85a95d5d2cb87f88bc3ec7c0564d1ab302e33ebf
Baeyeonggi <byg5913@gmail.com>
2021-01-26 12:29:49 +0900

    add apllo mutation

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -8,6 +8,7 @@ const GET_MOVIES = gql`
     movies {
       id
       medium_cover_image
+      isLiked @client
     }
   }
 `
@@ -71,11 +72,12 @@ const Home = () => {
             <Movie
               key={m.id}
               id={m.id}
+              isLiked={m.isLiked}
               bg={m.medium_cover_image}
             />
           ))}
         </Movies>
-      )}
+      )
     </Container>
   );
 };
